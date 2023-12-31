f38a563e9964e58c2037d0cd28a04cd5f9590206
usmarinec <112789792+usmarinec@users.noreply.github.com>
2023-12-30 21:43:38 -0700

    Added ResponseEntity to controllers (#12)

--- ledger-backend/src/main/java/com/financial/ledger/controllers/EntityController.java
+++ ledger-backend/src/main/java/com/financial/ledger/controllers/EntityController.java
@@ -3,7 +3,10 @@ package com.financial.ledger.controllers;
 import com.financial.ledger.domain.entity.Entity;
 import com.financial.ledger.service.entity.EntityService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +20,15 @@ public class EntityController {
   @Autowired private EntityService entityService;
 
   @PostMapping("/create")
-  public Entity createEntity(@RequestBody Entity entity) {
-    return entityService.saveEntity(entity);
+  public ResponseEntity<Entity> createEntity(@RequestBody Entity entity) {
+    Entity savedEntity = entityService.saveEntity(entity);
+    return new ResponseEntity<>(savedEntity, HttpStatus.CREATED);
   }
 
   @GetMapping("/fetch")
-  public List<Entity> getAllEntities() {
-    return entityService.getAllEntities();
+  public ResponseEntity<List<Entity>> getAllEntities() {
+    List<Entity> entities = entityService.getAllEntities();
+    return new ResponseEntity<>(entities, HttpStatus.OK);
   }
 
   /**
@@ -33,9 +38,11 @@ public class EntityController {
    * @return entity
    */
   @GetMapping("/fetch/{name}")
-  public Entity getEntityByName(@PathVariable String name) {
-    return entityService
-        .getEntityByName(name)
-        .orElseThrow(() -> new RuntimeException("Entity not found with name: " + name));
+  public ResponseEntity<Entity> getEntityByName(@PathVariable String name) {
+    Optional<Entity> optionalEntity = entityService.getEntityByName(name);
+
+    return optionalEntity
+        .map(entity -> new ResponseEntity<>(entity, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 }
--- ledger-backend/src/main/java/com/financial/ledger/controllers/je/EntryController.java
+++ ledger-backend/src/main/java/com/financial/ledger/controllers/je/EntryController.java
@@ -3,7 +3,10 @@ package com.financial.ledger.controllers.je;
 import com.financial.ledger.domain.je.Entry;
 import com.financial.ledger.service.je.EntryService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,18 +20,21 @@ public class EntryController {
   @Autowired EntryService entryService;
 
   @PostMapping("/create")
-  public Entry createEntry(@RequestBody Entry entry) {
-    return entryService.saveEntry(entry);
+  public ResponseEntity<Entry> createEntry(@RequestBody Entry entry) {
+    Entry savedEntry = entryService.saveEntry(entry);
+    return new ResponseEntity<>(savedEntry, HttpStatus.CREATED);
   }
 
   @PostMapping("/create-list")
-  public List<Entry> createEntries(@RequestBody List<Entry> entries) {
-    return entryService.saveEntries(entries);
+  public ResponseEntity<List<Entry>> createEntries(@RequestBody List<Entry> entries) {
+    List<Entry> savedEntries = entryService.saveEntries(entries);
+    return new ResponseEntity<>(savedEntries, HttpStatus.CREATED);
   }
 
   @GetMapping("/fetch")
-  public List<Entry> getAllEntries() {
-    return entryService.getAllEntries();
+  public ResponseEntity<List<Entry>> getAllEntries() {
+    List<Entry> entries = entryService.getAllEntries();
+    return new ResponseEntity<>(entries, HttpStatus.OK);
   }
 
   /**
@@ -38,9 +44,11 @@ public class EntryController {
    * @return entry
    */
   @GetMapping("/fetch/{id}")
-  public Entry getEntryById(@PathVariable String id) {
-    return entryService
-        .getEntryById(id)
-        .orElseThrow(() -> new RuntimeException("Entry not found with id: " + id));
+  public ResponseEntity<Entry> getEntryById(@PathVariable String id) {
+    Optional<Entry> optionalEntry = entryService.getEntryById(id);
+
+    return optionalEntry
+        .map(entry -> new ResponseEntity<>(entry, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 }
